06ba0c71b5640a40f3c2e2126e030128c270d5b1
Nikolay Mezuikha <nikolay.mezyuha@citeck.ru>
2023-02-13 01:22:49 +0700

    2469. Convert the Temperature - done

---
+++ src/main/java/org/nekomiz/problems/ConverttheTemperature2469/Solution.java
@@ -0,0 +1,10 @@
+package org.nekomiz.problems.ConverttheTemperature2469;
+
+class Solution {
+    public double[] convertTemperature(double celsius) {
+        return new double[]{
+                celsius + 273.15,
+                celsius * 1.80 + 32.00
+        };
+    }
+}
