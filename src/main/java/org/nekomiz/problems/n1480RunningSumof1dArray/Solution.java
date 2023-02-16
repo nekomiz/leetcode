eee5dcbfb0256946bc75ea1a5aada60ee52e0b87
Neki <slayraiz@gmail.com>
2023-02-16 22:58:14 +0700

    1480. Running Sum of 1d Array - Done (best solution);

--- src/main/java/org/nekomiz/problems/n1480RunningSumof1dArray/Solution.java
+++ src/main/java/org/nekomiz/problems/n1480RunningSumof1dArray/Solution.java
@@ -3,9 +3,16 @@ package org.nekomiz.problems.n1480RunningSumof1dArray;
 
 class Solution {
     public int[] runningSum(int[] nums) {
-        for (int i = 1; i < nums.length; i++) {
-            nums[i] += nums[i-1];
+        if (nums.length == 0) {
+            return nums;
         }
-        return nums;
+
+        int[] output = new int[nums.length];
+        output[0] = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            output[i] = output[i-1] + nums[i];
+        }
+        return output;
     }
 }
