13ad620d19e19e4db88064b4e28ba51114c9b44c
neki <slayraiz@gmail.com>
2023-03-05 12:48:17 +0700

    704. Binary Search - Done!

---
+++ src/main/java/org/nekomiz/problems/n704BinarySearch/Solution.java
@@ -0,0 +1,25 @@
+package org.nekomiz.problems.n704BinarySearch;
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int high = nums.length - 1;
+        int low = 0;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int num = nums[mid];
+
+            if (num == target) {
+                return mid;
+            }
+
+            if (num > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+}
